6f677f8ec4d246860fa95688fb00cc17681a65d8
Tutku Uçan <tutkuucan369@gmail.com>
2022-08-12 13:45:19 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<template lang="pug">
+main
+  a(href="http://google.com", target="_blank", rel="noopener noreferrer") 
+</template>
+
+<style lang="scss">
+main {
+  background: whitesmoke;
+  a {
+    color: red;
+  }
+}
+</style>
