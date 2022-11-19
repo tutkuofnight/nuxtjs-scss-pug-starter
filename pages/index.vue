fcb753075d45add2d980bb9fbf4d5ef3d361f44f
tutklon <mettheus0@gmail.com>
2022-11-19 17:57:46 +0300

    Update store and scss folders

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,18 @@
 <script>
+import {mapState} from 'vuex'
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  computed: {
+    ...mapState(['text'])
+  }
 }
 </script>
 
 <template lang="pug">
-main
-  h2 Hello Nuxt.js
+#app
+  h1 {{text}}
 </template>
 
 <style lang="scss">
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  text-decoration: none;
-  list-style: none;
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-}
-main {
-  background: whitesmoke;
-  padding: 10px;
-  h2 {
-    color: rgb(17, 131, 153);
-  }
-}
+
 </style>
